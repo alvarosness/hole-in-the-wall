62070569beb1d522e0f7938b2ee9edc29b8b7cc9
tsfarkas <tsfarkas@indiana.edu>
2017-04-26 00:54:57 -0400

    Update create_map.c

--- create_map.c
+++ create_map.c
@@ -2,19 +2,30 @@
 #include "functions.h"
 #include <curses.h>
 #include <stdlib.h>
+#include <string.h>
 
 void create_map(char map[][2000]){
   int col;
   int row, count;
+  int start_x, start_y;
+  String entry = "Press Any Key to Begin"  
+  start_x = COLS / 2 - 11;
+  start_y = LINES / 2;
 
 	for(col = 0; col < 2000; col++)
 		for(row = 0; row < LINES; row++)
 			map[row][col] = ' ';
-
+			
+  for(col = 0; col < 22; col++)
+     map[start_y][start_x + col] = *(entry + col);
 
   for(col = 0; col < 2000; col++)
   {
-    if(col < 100){
+    if(col < COLS){
+    	;
+    }
+    
+    else if(col < 100){
       if (col % 10 == 0 )
       {
         for( row = 0; row < LINES; row++)
