187f78b0fe0c1bdfab2b1880673ac8b8900b7aa5
jodoschr <jodoschr@iupui.edu>
2017-04-25 23:35:39 -0400

    Update single_player_mode.c

--- single_player_mode.c
+++ single_player_mode.c
@@ -2,6 +2,8 @@
 #include <curses.h>
 #include <stdlib.h>
 
+int mvflag = 0;
+
 void move_something(int * x, int * y){
   int num = rand() % 8;
   if (num == 0)
@@ -19,10 +21,13 @@ void single_player_mode(){
   char map[LINES][2000];
   int x, map_x;
   int y, map_y;
+  int delay = 200;
   int count = 0;
   create_map(map);
   x=0;
   y=0;
+  signal(SIGALRM, on_alarm);
+  set_ticker(delay);
    mvaddch(y,x,'>');
    refresh();
   while(1){
@@ -56,14 +61,17 @@ void single_player_mode(){
     for(map_x = 0; map_x < COLS; map_x++)
       for(map_y = 0; map_y < LINES; map_y++)
         mvaddch(map_y,map_x, map[map_y][map_x+count]);
- 
- 
-      mvaddch(y,x,'>');
+        mvaddch(y,x,'>');
 
-   
-
+   if(mvflag == 1){
+     count++;
+     mvflag = 0;
+   }
    refresh();
-    count++;
-    //sleep(1);
+  
+  
   }
+}
+void on_alarm(int signum){
+  mvflag = 1;
 }
