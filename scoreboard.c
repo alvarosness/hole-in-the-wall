9ae4b103521faea579b4cc56cda1b2e9bb494af8
Alvaro Esperanca <aesperan@indiana.edu>
2017-04-28 01:12:19 -0400

    Update scoreboard.c

--- scoreboard.c
+++ scoreboard.c
@@ -5,15 +5,31 @@ void scoreboard(){
   clear();
   int row = LINES/3;
   int col = COLS/3;
+  char topscore[10];
   
-  mvaddstr(row, col, "TOP SCORES");
-  mvaddstr(row+1, col, "1.1738");
-  mvaddstr(row+2, col, "2.420");
-  mvaddstr(row+3, col, "3.69");
-  mvaddstr(row+4, col, "4.42");
-  mvaddstr(row+5, col, "5.0");
+  if(read_score(topscore)){
+    mvaddstr(row, col, "TOP SCORE");
+    mvaddstr(row+1, col, topscore);
+  }else{
+    mvaddstr(row, col, "No Top Score Available");
+  }
   refresh();
   
   getch();
   
+}
+
+int read_score(char * str){
+  FILE * fd;
+  
+  fd = fopen("", "r");
+  
+  if(fd == NULL)
+    return 0;
+    
+  fgets(str,8,fd);
+  
+  fclose(fd);
+  
+  return 1;
 }
